1250cbafae3600cc05cb6e948d080efcd0d79afa
treymichaels <thomam7@uw.edu>
2020-11-03 17:05:12 -0800

    Remove use of Appbar component, replace with div

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,17 +2,19 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import './App.css';
-import { AppBar, Toolbar } from '@material-ui/core';
+import { Toolbar } from '@material-ui/core';
 
 function App() {
   return (
     <Router>
-      <AppBar position="fixed">
-        <Toolbar className="tool-bar">
-            <h1>BaseCheck</h1>
-            <a className="tool-link" href="/">Home</a>
-        </Toolbar>
-      </AppBar>
+      <header>
+        <div className="nav-bar">
+          <Toolbar className="tool-bar">
+              <h1>BaseCheck</h1>
+              <a className="tool-link" href="/">Home</a>
+          </Toolbar>
+        </div>
+      </header>
       <Switch>
           <Route exact path='/'><HomePage/></Route>
           <Route path='/county/'></Route>
@@ -25,6 +27,22 @@ function App() {
 
 function HomePage() {
     return (
+      <main className="home-page">
+        <div className="page-header">
+          <h2>COVID-19 Cases Today Across the Country</h2>
+          <p>BaseCheck wants to ensure that every person has the accessible opportunity to stay well-informed about the pandemic.</p>
+        </div>
+        <div>
+
+        </div>
+        <div>
+          <p>You have 0 saved locations right now</p>
+        </div>
+        <div>
+
+        </div>
+      </main>
+      /*
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -41,6 +59,7 @@ function HomePage() {
           </a>
         </header>
       </div>
+      */
     );
 }
 
