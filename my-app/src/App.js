580ecdc64e26e95a8055e98e2f85ac2fd1b31afd
treymichaels <thomam7@uw.edu>
2020-11-03 21:09:36 -0800

    Add search and api calls for county data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import './App.css';
@@ -26,15 +26,33 @@ function App() {
 }
 
 function HomePage() {
+
+  const baseUri = "https://disease.sh/v3/covid-19/jhucsse/counties/";
+  const [search, setSearch] = useState("");
+  const [counties, setCounties] = useState([]);
+  const [input, setInput] = useState("");
+
+  async function handleSearch() {
+    await fetch(baseUri + input)
+    .then((response) => response.json())
+    .then((responseData) => {
+      setSearch(input);
+      setCounties(responseData);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
   return (
     <main className="home-page">
       <div className="page-header">
         <h2>COVID-19 Cases Today Across the Country</h2>
         <p>BaseCheck wants to ensure that every person has the accessible opportunity to stay well-informed about the pandemic.</p>
+        <input type="text" value={input} placeHolder="search for a county" onChange={e => setInput(e.target.value)}></input>
+        <button onClick={handleSearch}>Search!</button>
       </div>
-      <CountyCardList/>
+      {search !== "" ? <CountyCardList counties={counties} search={search}/> : <CountyCardList/>}
       <div>
-
       </div>
     </main>
   );
@@ -43,35 +61,45 @@ function HomePage() {
 /*
   Component that will act a a container to hold each CountyCard Component.
 */
-function CountyCardList() {
-  return (
-    <div>
-      <p>You have 0 saved locations right now</p>
-      <div>
-
+function CountyCardList(props) {
+  if (props.counties) {
+    if (props.counties.length > 0) {
+      return (
+        <div className="list">
+        <p>{props.counties.length} search results found...</p>
+        <div className="card-container">
+  
+        </div>
       </div>
-    </div>
-  );
+      );
+    } else {
+      return (
+        <div className="list">
+        <p>Unfortunately, we have no results for "{props.search}"</p>
+        </div>
+      );
+    }
+  } else  {
+    return (
+      <p>Hello!</p>
+    );
+  }
 }
 
 /*
   Component that represents a County Page.
 */
-function CountyDetail() {
+function CountyDetail(props) {
   return (
-    /*
-    <main>
-    </main>
-    */
     <main className="county-page">
       <div className="county-header">
-          <h2>King County, WA</h2>
-          <p><b>Risk Level: High</b></p>
+          <h2>{props.county} County, {props.state}</h2>
+          <p><b>Risk Level: {props.risk}</b></p>
       </div>
       <div className="county-body">
         <div className="county-main">
           <div className="county-stats">
-            <p>New Cases (1 Day):</p>
+            <p>New Cases (1 Day): </p>
             <p>New Cases (7 Days):</p>
             <p>Total Cases:</p>
             <p>Total Deaths:</p>
@@ -79,7 +107,6 @@ function CountyDetail() {
           </div>
           <div className="county-visual"></div>
         </div>
-        <div></div>
       </div>
     </main>
   );
