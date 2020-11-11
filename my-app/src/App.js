7e63c1aa1e0189345d7effd579bf54f27c28795e
treymichaels <thomam7@uw.edu>
2020-11-10 18:01:57 -0800

    Refactor countydetail out of app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect, useParams } from 'react-router-dom';
-import LineGraph from './components/LineGraph';
 import './App.css';
+import CountyDetail from './components/CountyDetail';
 import { Toolbar } from '@material-ui/core';
 
 function App() {
@@ -110,69 +110,6 @@ function CountyCardList(props) {
       <div></div>
     );
   }
-}
-
-/*
-  Component that represents a County Page.
-*/
-function CountyDetail(props) {
-
-  let { county, state } = useParams();
-  county = county.charAt(0).toUpperCase() + county.slice(1);
-  state = state.charAt(0).toUpperCase() + state.slice(1);
-  const [location, setLocation] = useState();
-  const [hasCounty, setHasCounty] = useState(false);
-  const requestUri = "https://cors-anywhere.herokuapp.com/https://covercovid-19.com/county/" + county + "/" + state;
-
-  useEffect(() => {
-    fetch(requestUri).then((response) => response.json())
-    .then((responseData) => {
-      let data = responseData[0];
-      setLocation(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }, []);
-
-
-  if (!location) {
-    return (
-      <div></div>
-    );
-  }
-  return (
-    <main className="more-info">
-      <div className="county-page">
-        <div className="county-header">
-            <div>
-              <a className="back" href={'/search/' + county}>Back</a>
-            </div>
-            <div>
-              <h2>{county} County, {state}</h2>
-              <p><b>Risk Level: {props.risk}</b></p>
-            </div>
-            <div className="favorite">
-              <button>Like</button>
-            </div>
-        </div>
-        <div className="county-body">
-          <div className="county-main">
-            <div className="county-stats">
-              <p>Total Cases: <b>{location ? location["cnt"] : "N/A"}</b></p>
-              <p>New Cases (2 Weeks): <b>{location ? location["2wd"] : "N/A"}</b></p>
-              <p>New Cases (1 Week): <b>{location ? location["1wd"] : "N/A"}</b></p>
-              <p>New Cases (1 Day): <b>{location ? location["1dd"] : "N/A"}</b></p>
-              <p>Last Updated: <b>{location ? location.date.split(" ")[0] : "N/A"}</b></p>
-            </div>
-            <div className="county-visual">
-              {location ? <LineGraph location={location}/> : <div></div>}
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
 }
 
 export default App;
