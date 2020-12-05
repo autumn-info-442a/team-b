5b2e3e7b68ef0afa3b7659b3c8e6c5aa8ffdad38
treymichaels <thomam7@uw.edu>
2020-12-05 12:01:35 -0800

    US Dashboard now uses real data

--- my-app/src/components/UsDashboard.js
+++ my-app/src/components/UsDashboard.js
@@ -1,10 +1,22 @@
-//import React, { useState } from 'react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 /*
   Component that represents a US Dashboard on Home Page.
 */
 export default function UsDashboard() {
+    const requestUri = "https://disease.sh/v3/covid-19/countries/us?yesterday=true";
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch(requestUri).then((response) => response.json())
+        .then((responseData) => {
+            setData(responseData);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }, []);
+
     return (
         <div className="us-page">
             <div>
@@ -12,20 +24,20 @@ export default function UsDashboard() {
             </div>
             <div className="us-stats">
                 <div className="us-individual">
-                    <p>TOTAL CASES: </p>
-                    <div className="us-cases"><p>7,835,007</p></div>
+                    <p>TOTAL CASES:</p>
+                    <div className="us-cases"><p>{data['cases']}</p></div>
                 </div>
                 <div className="us-individual">
-                    <p>NEW CASES (1 Week):</p>
-                    <div className="us-cases"><p>359,835</p></div>
+                    <p>NEW CASES (1 Day):</p>
+                    <div className="us-cases"><p>{data['todayCases']}</p></div>
                 </div>
                 <div className="us-individual">
-                    <p>TOTAL DEATH: </p>
-                    <div className="us-cases"><p>215,194</p></div>
+                    <p>TOTAL DEATHS:</p>
+                    <div className="us-cases"><p>{data['deaths']}</p></div>
                 </div>
             </div>
             <div className="us-update">
-                <p>CDC | Updated: Oct 14 2020 12:21PM</p>
+                <p>Worldometers | Updated Every 10 Minutes</p>
             </div>
         </div>
       );
